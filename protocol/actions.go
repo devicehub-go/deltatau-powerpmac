88bf2d8bf41ba4c44207e3cee86624b293790e48
Leonardo Rossi Leao <leonardo.leao@cnpem.br>
2026-03-10 12:43:56 -0300

    feat: updating action methods to enable multi-motors commands

--- protocol/actions.go
+++ protocol/actions.go
@@ -1,75 +1,110 @@
 package protocol
 
-import "fmt"
-
-type JodeMode string
-
-const (
-	JogPlus  = "+"
-	JogMinus = "-"
-	JogStop  = "/"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
+type JogMode string
+
+const (
+	JogPlus  JogMode = "+"
+	JogMinus JogMode = "-"
+	JogStop  JogMode = "/"
+)
+
+func motorIDs(ids []int) (string, error) {
+	if len(ids) == 0 {
+		return "", errors.New("at least one motor id is required")
+	}
+	parts := make([]string, len(ids))
+	for i, id := range ids {
+		parts[i] = strconv.Itoa(id)
+	}
+	return strings.Join(parts, ","), nil
+}
+
 // Start/Stop an indefinite jogging
-func (p *PowerPMAC) Jog(id int, mode JodeMode) error {
-	command := fmt.Sprintf("#%djog%s", id, mode)
-	_, err := p.Request(command)
+func (p *PowerPMAC) Jog(mode JogMode, ids ...int) error {
+	m, err := motorIDs(ids)
+	if err != nil {
+		return err
+	}
+	_, err = p.Request(fmt.Sprintf("#%sjog%s", m, mode))
 	return err
 }
 
 // Starts an absolute jog to a specific position
-func (p *PowerPMAC) JogAbsolute(id int, position float64) error {
-	command := fmt.Sprintf("#%djog=%f", id, position)
-	_, err := p.Request(command)
+func (p *PowerPMAC) JogAbsolute(position float64, ids ...int) error {
+	m, err := motorIDs(ids)
+	if err != nil {
+		return err
+	}
+	_, err = p.Request(fmt.Sprintf("#%sjog=%f", m, position))
 	return err
 }
 
 // Starts a relative jog by a specified distance from the current
 // commanded position
-func (p *PowerPMAC) JogRelative(id int, distance float64) error {
-	command := fmt.Sprintf("#%djog:%f", id, distance)
-	_, err := p.Request(command)
+func (p *PowerPMAC) JogRelative(distance float64, ids ...int) error {
+	m, err := motorIDs(ids)
+	if err != nil {
+		return err
+	}
+	_, err = p.Request(fmt.Sprintf("#%sjog:%f", m, distance))
 	return err
 }
 
 // Start an absolute jog for the specified coordinate system
 func (p *PowerPMAC) MoveAxisAbsolute(coord int, axis string, target float64) error {
-	command := fmt.Sprintf("&%dcx%s%f", coord, axis, target)
-	_, err := p.Request(command)
+	_, err := p.Request(fmt.Sprintf("&%dcx%s%f", coord, axis, target))
 	return err
 }
 
 // Starts a homing-search move to establish an absolute position reference
-func (p *PowerPMAC) Home(id int) error {
-	command := fmt.Sprintf("#%dhome", id)
-	_, err := p.Request(command)
+func (p *PowerPMAC) Home(ids ...int) error {
+	m, err := motorIDs(ids)
+	if err != nil {
+		return err
+	}
+	_, err = p.Request(fmt.Sprintf("#%shome", m))
 	return err
 }
 
 // Establishes the current position as zero without motion
-func (p *PowerPMAC) ZeroMoveHome(id int) error {
-	command := fmt.Sprintf("#%dhomez", id)
-	_, err := p.Request(command)
+func (p *PowerPMAC) ZeroMoveHome(ids ...int) error {
+	m, err := motorIDs(ids)
+	if err != nil {
+		return err
+	}
+	_, err = p.Request(fmt.Sprintf("#%shomez", m))
 	return err
 }
 
 // Immediately disables servo control, opens the loop, and cuts power
-func (p *PowerPMAC) Kill(id int) error {
-	command := fmt.Sprintf("#%dkill", id)
-	_, err := p.Request(command)
+func (p *PowerPMAC) Kill(ids ...int) error {
+	m, err := motorIDs(ids)
+	if err != nil {
+		return err
+	}
+	_, err = p.Request(fmt.Sprintf("#%skill", m))
 	return err
 }
 
 // Engaging the brake and waiting before disabling power
-func (p *PowerPMAC) DelayedKill(id int) error {
-	command := fmt.Sprintf("#%ddkill", id)
-	_, err := p.Request(command)
+func (p *PowerPMAC) DelayedKill(ids ...int) error {
+	m, err := motorIDs(ids)
+	if err != nil {
+		return err
+	}
+	_, err = p.Request(fmt.Sprintf("#%sdkill", m))
 	return err
 }
 
 // Starts the execution of a Script PLC Program
 func (p *PowerPMAC) EnablePLC(name string) error {
-	command := fmt.Sprintf("enable plc %s", name)
-	_, err := p.Request(command)
+	_, err := p.Request(fmt.Sprintf("enable plc %s", name))
 	return err
 }
