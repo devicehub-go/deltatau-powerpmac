1f5c89334e31fc528e890f54da1fff05f06cbc6b
Leonardo Rossi Leao <leonardo.leao@cnpem.br>
2026-03-10 13:14:19 -0300

    feat: adding a jog axis relative command

--- protocol/actions.go
+++ protocol/actions.go
@@ -58,8 +58,14 @@ func (p *PowerPMAC) JogRelative(ids []int, distance float64) error {
 }
 
 // Start an absolute jog for the specified coordinate system
-func (p *PowerPMAC) MoveAxisAbsolute(coord int, axis string, target float64) error {
-	_, err := p.Request(fmt.Sprintf("&%dcx%s%f", coord, axis, target))
+func (p *PowerPMAC) JogAxisAbsolute(coord int, axis string, position float64) error {
+	_, err := p.Request(fmt.Sprintf("&%d abs cx %s%f", coord, axis, position))
+	return err
+}
+
+// Start a relative jog for the specified coordinate system
+func (p *PowerPMAC) JogAxisRelative(coord int, axis string, distance float64) error {
+	_, err := p.Request(fmt.Sprintf("&%d abs cx %s%f", coord, axis, distance))
 	return err
 }
 
